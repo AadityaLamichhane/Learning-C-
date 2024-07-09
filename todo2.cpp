2db65f2db5847b0d5d6d2a39ca507fdcca077d6b
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2024-07-09 16:22:26 +0545

    asas

---
+++ todo2.cpp
@@ -0,0 +1,12 @@
+//Wap to add two objct of different class with the data member feet and inch my using the member function 
+
+#include<iostream>
+using namespace std;
+class length{
+	public:
+		 float feet;
+		 
+		 void display(){
+		 	cout<<display
+		 }
+};
