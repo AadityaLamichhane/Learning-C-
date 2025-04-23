2db3366627f99d01289cd41e4bae5997afe5a862
Aaditya lamichhane <lamichhaneaaditya01@gmail.com>
2025-04-23 19:56:59 +0545

    changes

---
+++ sem 3 (DSA)/practise.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int value;
+    cout<<"Enter the number you want to entner to the array "<<endl;
+    cin>>value ; 
+// Make the node with the total number of the 
+    return 0 ; 
+}
